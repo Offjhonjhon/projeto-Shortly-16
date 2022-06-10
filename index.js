daf626c19244e87c95496056bb1011bdd3181282
Offjhonjhon <jjvictorac@gmail.com>
2022-06-10 15:39:25 -0300

    prepare to deploy

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(cors());
 app.use(json());
 app.use(router);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`server is up and running onport ${port}`);
-})
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
