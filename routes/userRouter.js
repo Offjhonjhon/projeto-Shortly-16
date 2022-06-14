fe3476b2dff662ba47c7ed56e6667f503da00e2c
Vinicius <vinigugelmin@hotmail.com>
2022-06-14 11:34:44 -0300

    Preparando revisão de código

--- routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-
-import { getUsersId } from '../controllers/usersController.js';
-
-const userRouter = Router();
-userRouter.get('/users/:id', getUsersId);
-
-export default userRouter;
